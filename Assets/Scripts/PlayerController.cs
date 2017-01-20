873fb09e9c5bc69c0d4910ca9ac90bc0d990ea53
sionl <sion.lloyd@mccann.com>
2017-01-20 23:09:31 +0000

    Add Sceen and chaneg player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,10 @@ using System.Collections;
 [System.Serializable]
 public class Boundary
 {
-    public float xMin, xMax, zMin, zMax;
+    public float xMin = -100;
+    public float xMax = 100;
+    public float zMin = -100;
+    public float zMax = 100;
 }
 
 public class PlayerController : MonoBehaviour
@@ -13,22 +16,24 @@ public class PlayerController : MonoBehaviour
     public float tilt;
     public Boundary boundary;
 
-    void FixedUpdate ()
+    void FixedUpdate()
     {
-		Rigidbody rb = GetComponent<Rigidbody>();
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
+        Rigidbody rb = GetComponent<Rigidbody>();
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		rb.velocity = movement * speed;
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * speed;
 
-		rb.position = new Vector3 
+
+
+        rb.position = new Vector3
         (
-            Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax), 
-            0.0f, 
-            Mathf.Clamp (rb.position.z, boundary.zMin, boundary.zMax)
+            Mathf.Clamp(rb.position.x + 1, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z + 1, boundary.zMin, boundary.zMax)
         );
 
-        rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
     }
 }
