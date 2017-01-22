f312223871d33116cb83050da890b4a2270e44d9
Alex Knowles <Alex@knowlesy.co.uk>
2017-01-22 00:23:49 +0000

    Planets

--- Assets/Scripts/PlanetController.cs
+++ Assets/Scripts/PlanetController.cs
@@ -1,21 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlanetController : MonoBehaviour
 {
-    List<GameObject> Planets;
+    private List<GameObject> Planets;
+    private GameManager GameManagerReference;
+    float intervalToNextPlanet;
 
     // Use this for initialization
     void Start()
     {
         Planets = new List<GameObject>();
-        CreatePlanet(Random.Range(1, 3));
+        GameManagerReference = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        CreatePlanet(3);
+
+        intervalToNextPlanet = Time.time + (Random.value * 10);
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        if (Time.time > intervalToNextPlanet)
+            CreatePlanet(Mathf.CeilToInt(Random.Range(1f, 3f)));
+
+        Vector3 bottomOfScreen = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 20));
+
+        List<GameObject> itemsToRemove = new List<GameObject>();
+
+        foreach (GameObject planet in Planets)
+        {
+            if (planet.transform.position.y + planet.GetComponent<Collider>().bounds.size.y < bottomOfScreen.y )                
+                itemsToRemove.Add(planet);
+        }
+
+        foreach (GameObject planet in itemsToRemove)
+        {
+            Planets.Remove(planet);
+            Destroy(planet);
+        }
     }
 
     private void CreatePlanet(int _Type)
@@ -25,11 +49,9 @@ public class PlanetController : MonoBehaviour
 
         float xSeed = Random.Range(0, Screen.width);
         Vector3 newItemPosition = Camera.main.ScreenToWorldPoint(new Vector3(xSeed, Screen.height, 20));
-
-        newItemPosition.y -= newPlanet.GetComponent<Collider>().bounds.size.y;
-
-        Instantiate(newPlanet, newItemPosition, Quaternion.identity, gameObject.transform);
-
-        Planets.Add(newPlanet);
+        
+        Planets.Add(Instantiate(newPlanet, newItemPosition, Quaternion.identity, gameObject.transform));
+        
+        intervalToNextPlanet = Time.time + (Random.value * (10 - Mathf.FloorToInt(GameManagerReference.Score / 1000)));
     }
 }
